2fb7b353dd0ea18879d78c42f9f70ead74086e83
Taha-Hasemi <kiziltahaaslan@gmail.com>
2021-10-02 15:03:43 +0330

    Son Commit

--- MVCProjeKampi/Controllers/GraphicsController.cs
+++ MVCProjeKampi/Controllers/GraphicsController.cs
@@ -1,6 +1,7 @@
 ﻿using BusinessLayer.Concrete;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using MVCProjeKampi.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,7 @@ namespace MVCProjeKampi.Controllers
         // GET: Graphics
         HeadingManager headingManager = new HeadingManager(new EfHeadingDal());
         CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+        ContentManager contentManager = new ContentManager(new EfContentDal());
 
         public ActionResult Index()
         {
@@ -23,17 +25,36 @@ namespace MVCProjeKampi.Controllers
         {
             return View();
         }
+        public ActionResult HeadingChartIndex()
+        {
+            return View();
+        }
         public ActionResult CategoryChart()
         {
             return Json(GetCategoryStatus(), JsonRequestBehavior.AllowGet);
         }
-        public List<Category> GetCategoryStatus()
+        public ActionResult HeadingChart()
+        {
+            return Json(GetHeadingStatus(), JsonRequestBehavior.AllowGet);
+        }
+        public List<CategoryChart> GetCategoryStatus()
         {
             var values= headingManager.List().GroupBy(x => x.CategoryID).SelectMany(x => categoryManager.List().Where(y => y.CategoryID == x.Key).ToList() , (x,y)=>new { Key = y.CategoryName, Count = x.Count() }).ToList();
-            List<Category> values2 = new List<Category>();
+            List<CategoryChart> values2 = new List<CategoryChart>();
             foreach (var item in values)
             {
-                values2.Add(new Category { CategoryID = Convert.ToInt32(item.Count), CategoryName = item.Key.ToString() });
+                values2.Add(new CategoryChart { Count = Convert.ToInt32(item.Count), Name = item.Key.ToString() });
+            }
+
+            return values2;
+        }
+        public List<HeadingChart> GetHeadingStatus()
+        {
+            var values = contentManager.List().GroupBy(x => x.Heading.HeadingName).ToList();
+            List<HeadingChart> values2 = new List<HeadingChart>();
+            foreach (var item in values)
+            {
+                values2.Add(new HeadingChart { Count = Convert.ToInt32(item.Count()), Name = item.Key.ToString() });
             }
 
             return values2;
